3681c60b256a0d73376d8208f5032683908944c9
MarekUlip <marek.ulip@gmail.com>
2019-12-04 15:29:51 +0100

    Updated text generators for hyperas testing.

--- text_generators/text_generator.py
+++ text_generators/text_generator.py
@@ -6,7 +6,7 @@ from helper_functions import preprocess_sentence
 
 class TextGenerator(Sequence):
     def __init__(self, filename, batch_size, num_of_texts, num_of_words, tokenizer: Tokenizer, delimeter,
-                 num_of_classes, max_len=None, start_point=0, preprocess=False):
+                 num_of_classes, max_len=None, start_point=0, preprocess=False,preload_dataset=True, is_predicting=False):
         self.filename = filename
         self.batch_size = batch_size
         self.num_of_texts = num_of_texts
@@ -17,22 +17,38 @@ class TextGenerator(Sequence):
         self.start_point = start_point
         self.max_len = max_len
         self.preprocess = preprocess
+        self.preload_dataset = preload_dataset
+        self.is_predicting = is_predicting
+        self.labels = None
+        self.tmp_articles = None
         self.articles = None
+        if preload_dataset:
+            self.load_dataset()
 
     def __len__(self):
         return np.ceil(self.num_of_texts / self.batch_size).astype(np.int)
 
-    def __getitem__(self, item):
-        self.articles = []
+    def load_dataset(self):
         with open(self.filename, encoding='utf-8', errors='ignore') as csvfile:
-            for row in islice(csv.reader(csvfile, delimiter=self.delimeter), self.start_point+item*self.batch_size,None):
-                self.articles.append([int(row[0]),preprocess_sentence(row[1])])
-                if len(self.articles) >= self.batch_size:
-                    break
+            for row in csv.reader(csvfile, delimiter=self.delimeter):
+                self.articles.append([int(row[0]),preprocess_sentence(row[1]) if self.preprocess else row[1]])
 
-        self.articles = np.array(self.articles)
-        if len(self.articles.shape) < 2:
+    def __getitem__(self, item):
+        self.tmp_articles = []
+        if self.preload_dataset:
+            self.tmp_articles = self.articles[self.start_point+item*self.batch_size:self.start_point+(item+1)*self.batch_size]
+        else:
+            with open(self.filename, encoding='utf-8', errors='ignore') as csvfile:
+                for row in islice(csv.reader(csvfile, delimiter=self.delimeter), self.start_point+item*self.batch_size,None):
+                    self.tmp_articles.append([int(row[0]),preprocess_sentence(row[1])])
+                    if len(self.tmp_articles) >= self.batch_size:
+                        break
+
+        self.tmp_articles = np.array(self.tmp_articles)
+        if len(self.tmp_articles.shape) < 2:
             print("Working around...")
-            self.articles = np.array([[0, "fgdssdgdsfgdsfgdsfg"], [1, "fgdssdgdsfgdsfgdsfg"]])
+            self.tmp_articles = np.array([[0, "fgdssdgdsfgdsfgdsfg"]])
+        if self.is_predicting:
+            self.labels.extend(list(map(int,self.tmp_articles[:,0])))
 
 
--- text_generators/training_text_generator_RNN.py
+++ text_generators/training_text_generator_RNN.py
@@ -4,13 +4,16 @@ from text_generators.text_generator import TextGenerator
 
 class TrainingTextGeneratorRNN(TextGenerator):
     def __init__(self, filename, batch_size, num_of_texts, num_of_words, tokenizer: Tokenizer, delimeter,
-                 num_of_classes, max_len=None, start_point=0, preprocess=False):
+                 num_of_classes, max_len=None, start_point=0, preprocess=False,preload_dataset=True, is_predicting=False):
         super().__init__(filename, batch_size, num_of_texts, num_of_words, tokenizer, delimeter, num_of_classes,
-                         max_len, start_point, preprocess)
+                         max_len, start_point, preprocess,preload_dataset,is_predicting)
 
     def __getitem__(self, item):
         super().__getitem__(item)
-        labels = to_categorical(self.articles[:,0], num_classes=self.num_of_classes, dtype=np.uint8)
-        features = self.tokenizer.texts_to_matrix(self.articles[:,1],mode="binary")
-        self.articles = None
-        return np.reshape(features,(features.shape[0], 1, features.shape[1])), labels
+        labels = to_categorical(self.tmp_articles[:,0], num_classes=self.num_of_classes, dtype=np.uint8)
+        features = self.tokenizer.texts_to_matrix(self.tmp_articles[:,1],mode="binary")
+        self.tmp_articles = None
+        if self.is_predicting:
+            return np.reshape(features,(features.shape[0], 1, features.shape[1]))
+        else:
+            return np.reshape(features,(features.shape[0], 1, features.shape[1])), labels

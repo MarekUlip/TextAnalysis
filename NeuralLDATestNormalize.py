7aae2bb8ca6652aeb98bff8063da0af1c09a1112
MarekUlip <marek.ulip@gmail.com>
2020-02-16 18:27:03 +0100

    Added more topic extraction models that use regularization, tfidf or normalization.

--- NeuralLDATestNormalize.py
+++ NeuralLDATestNormalize.py
@@ -286,7 +286,7 @@ test_name = simpledialog.askstring(title="Test Name",
 #results_saver = LogWriter(log_file_desc="Autoencoder")
 results = []
 #mycolors = np.array([color for name, color in mcolors.XKCD_COLORS.items()])
-from sys import getsizeof
+
 num_of_words = 10000
 dataset_helper = Dataset_Helper(True)
 dataset_helper.set_wanted_datasets([2])
@@ -299,9 +299,7 @@ tokenizer.fit_on_texts(documents)
 #items= tokenizer.word_index
 reverse_word_map = dict(map(reversed, tokenizer.word_index.items()))
 matrix = tokenizer.texts_to_matrix(documents, mode='binary')
-print(getsizeof(documents))
-print(getsizeof(tokenizer))
-print(getsizeof(matrix))
+
 #mydict = corpora.Dictionary([line.split() for line in documents],prune_at=num_of_words)
 #corpus = [mydict.doc2bow(line.split()) for line in documents]
 
@@ -313,9 +311,11 @@ model.add(Dense(num_of_words*num_of_topics,activation='relu', input_shape=(num_o
 model.add(Dense(num_of_words,activation='sigmoid'))"""
 regularization = 0.001
 input_row = Input(shape=(num_of_words,))
+encoder = keras.layers.LayerNormalization()(input_row)
 #encoder = Dense(int(num_of_words/2), activation='relu')(input_row)
-encoder= Dense(num_of_topics, activation='relu', activity_regularizer=keras.regularizers.l1_l2(regularization))(input_row)#
+encoder= Dense(num_of_topics, activation='relu', activity_regularizer=keras.regularizers.l1_l2(regularization))(encoder)#
 #encoder= keras.layers.LayerNormalization()(encoder)
+#encoder = Dropout(0.35)(encoder)
 #decoder = Dense(int(num_of_words/2), activation='relu')(encoder)
 output_row = Dense(num_of_words,activation='sigmoid')(encoder)
 
@@ -324,12 +324,12 @@ autoencoder.summary()
 #autoencoder.compile(optimizer='adadelta', loss='mse', metrics=['accuracy'])
 autoencoder.compile(optimizer='adam', loss='binary_crossentropy',metrics=['accuracy'])#optimizer='adadelta', loss='mse', metrics=['accuracy'])
 #autoencoder.compile(optimizer='adadelta', loss='categorical_crossentropy', metrics=['accuracy'])
-history = autoencoder.fit(matrix,matrix,batch_size=32,epochs=1000,validation_split=0.1, verbose=2, callbacks=[EarlyStopping(monitor='val_loss', min_delta=0, patience=50, verbose=0, mode='auto', baseline=None, restore_best_weights=False)])
-weight_in = autoencoder.get_weights()[0]
-weight_out = autoencoder.get_weights()[2]
-tst = autoencoder.get_weights()
+history = autoencoder.fit(matrix,matrix,batch_size=32,epochs=1000,validation_split=0.1, verbose=2, callbacks=[EarlyStopping(monitor='val_loss', min_delta=0, patience=1000, verbose=0, mode='auto', baseline=None, restore_best_weights=False)])
+weight_in = autoencoder.get_weights()[2]
+weight_out = autoencoder.get_weights()[4]
 blob = np.array([])
 weight_in = weight_in.transpose()
+#tst = autoencoder.get_weights()
 num_of_important_words = 20
 from results_saver import LogWriter
 
@@ -367,7 +367,7 @@ doc_term_matrix = [dictionary.doc2bow(doc) for doc in texts]"""
 model = Lda(num_of_topics,num_of_important_words,
             passes=4,
             iterations=4)
-
+model.train(documents)
 """gensim.models.LdaModel(
 doc_term_matrix,
 num_topics=num_of_topics,
@@ -375,33 +375,25 @@ id2word=dictionary,
 passes=2,
 iterations=2)"""
 
-#LDA section
-test_LDA = False
-if test_LDA:
-    model.train(documents)
-    topic_words_lda = extract_important_words(model.get_topics(), True)
-    print(topic_words_lda)
-    log_writer.write_2D_list('topic_words_lda', topic_words_lda, 'w+')
-    test_model(documents, labels, model, log_writer, 'standard_lda')
-    plot_clustering_chart(model,True,documents,log_writer,'lda',dataset_helper.get_dataset_name())
-    measureCoherence(topic_words_lda, log_writer, model.dictionary, documents, 'lda', dataset_helper.get_dataset_name())
-else:
-    model.dictionary = corpora.Dictionary([text.split() for text in documents])
+topic_words_lda = extract_important_words(model.get_topics(), True)
+print(topic_words_lda)
+log_writer.write_2D_list('topic_words_lda', topic_words_lda, 'w+')
 neural_lda_in = NeuralTopicMatrix(weight_in,reverse_word_map,num_of_topics,tokenizer)
 neural_lda_out = NeuralTopicMatrix(weight_out,reverse_word_map,num_of_topics,tokenizer)
 test_model(documents, labels, neural_lda_in, log_writer,'neural_lda_in')
 test_model(documents, labels, neural_lda_out, log_writer,'neural_lda_out')
+test_model(documents, labels, model, log_writer,'standard_lda')
+
+plot_clustering_chart(model,True,documents,log_writer,'lda',dataset_helper.get_dataset_name())
+plot_clustering_chart(neural_lda_in,False,documents,log_writer,'neural_topic_in',dataset_helper.get_dataset_name())
+plot_clustering_chart(neural_lda_out,False,documents,log_writer,'neural_topic_out',dataset_helper.get_dataset_name())
 
 
+measureCoherence(topic_words_lda,log_writer,model.dictionary,documents,'lda',dataset_helper.get_dataset_name())
 measureCoherence(topic_words_in_max,log_writer,model.dictionary,documents,'neural_in_max',dataset_helper.get_dataset_name())
 measureCoherence(topic_words_in_min,log_writer,model.dictionary,documents,'neural_in_min',dataset_helper.get_dataset_name())
 measureCoherence(topic_words_out_max,log_writer,model.dictionary,documents,'neural_out_max',dataset_helper.get_dataset_name())
 measureCoherence(topic_words_out_min,log_writer,model.dictionary,documents,'neural_out_min',dataset_helper.get_dataset_name())
-
-plot_clustering_chart(neural_lda_out,False,documents,log_writer,'neural_topic_out',dataset_helper.get_dataset_name())
-plot_clustering_chart(neural_lda_in,False,documents,log_writer,'neural_topic_in',dataset_helper.get_dataset_name())
-
-
 log_writer.end_logging()
 
 #print(topic_words)

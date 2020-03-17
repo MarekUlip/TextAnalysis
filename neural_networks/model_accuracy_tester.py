10a1e90f60aa58506ec551b075f426db7b3984f6
MarekUlip <marek.ulip@gmail.com>
2020-03-17 20:28:10 +0100

    Optimized more models.

--- neural_networks/model_accuracy_tester.py
+++ neural_networks/model_accuracy_tester.py
@@ -19,8 +19,9 @@ class ModelType(Enum):
     BIDI_GRU = 4
     CONV = 5
     CONV_GRU = 6
-    EMBEDDING_LSTM = 7
-    EMBEDDING_TRAINED_LSTM = 8
+    EMBEDDING_GLOVE_LSTM = 7
+    EMBEDDING_GLOVE_TRAINED_LSTM = 8
+    EMBEDDING_TRAINED_LSTM = 9
 
 def get_LSTM_model(datasets_helper, params=None):
     model: Sequential = Sequential()
@@ -54,7 +55,7 @@ def get_RNN_model(datasets_helper, params=None):
     model = Sequential()
     model.add(SimpleRNN(64, input_shape=(1, num_of_words), return_sequences=True))
     model.add(keras.layers.GaussianNoise(0.3))
-    model.add(SimpleRNN(64, activation='relu'))
+    model.add(SimpleRNN(64))
     model.add(keras.layers.LayerNormalization())
     model.add(Dense(datasets_helper.get_num_of_topics(), activation='softmax'))
 
@@ -65,7 +66,7 @@ def get_GRU_model(datasets_helper, params=None):
     model = Sequential()
     model.add(GRU(64, input_shape=(1, num_of_words), return_sequences=True))
     model.add(keras.layers.GaussianNoise(0.3))
-    model.add(GRU(64, activation='relu'))
+    model.add(GRU(64))
     model.add(keras.layers.LayerNormalization())
     model.add(Dense(datasets_helper.get_num_of_topics(), activation='softmax'))
 
@@ -73,24 +74,21 @@ def get_GRU_model(datasets_helper, params=None):
     return model
 
 def get_conv_model(datasets_helper, params=None):
-    embedding_dim = 100
+    embedding_dim = params.get('max_seq_len',200)
     tokenizer = params.get('tokenizer',None)
-    embedding_layer = Embedding(len(tokenizer.word_index) + 1,
-                                embedding_dim,
-                                weights=[get_embedding_matrix(num_of_words, embedding_dim, tokenizer.word_index)],
-                                input_length=embedding_dim,
-                                trainable=False)
-    sequence_input = Input(shape=(embedding_dim,), dtype='int32')
-    embedded_sequences = embedding_layer(sequence_input)
-    l_cov1 = Conv1D(128, 5, activation='relu')(embedded_sequences)
-    l_pool1 = MaxPooling1D(5)(l_cov1)
-    l_cov2 = Conv1D(128, 5, activation='relu')(l_pool1)
-    l_pool2 = MaxPooling1D(5)(l_cov2)
-    l_cov3 = Conv1D(128, 5, activation='relu')(l_pool2)
-    l_pool3 = MaxPooling1D(35)(l_cov3)  # global max pooling
-    l_flat = Flatten()(l_pool3)
-    l_dense = Dense(128, activation='relu')(l_flat)
-    preds = Dense(datasets_helper.get_num_of_topics(), activation='softmax')(l_dense)
+    max_seq_len = params.get('max_seq_len',400)
+
+    model = Sequential()
+    model.add(Embedding(15000, embedding_dim, input_length=max_seq_len))
+    model.add(Conv1D(128, 5, activation='relu'))
+    model.add(keras.layers.GlobalMaxPooling1D())
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dense(datasets_helper.get_num_of_topics(), activation='softmax'))
+    model.compile(optimizer='adam',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+    return model
 
 def get_conv_gru_model(datasets_helper, params=None):
     model = Sequential()
@@ -98,41 +96,55 @@ def get_conv_gru_model(datasets_helper, params=None):
                             input_shape=(1,num_of_words)))
     model.add(MaxPooling1D(1))
     model.add(Conv1D(40, 1, activation='relu'))
-    model.add(GRU(40, dropout=0.1, recurrent_dropout=0.5))
+    model.add(GRU(40, dropout=0.1))
     model.add(Dense(datasets_helper.get_num_of_topics(),activation='softmax'))
 
-    model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
     return model
 
 def get_bidi_model(datasets_helper, params=None):
     model = Sequential()
-    enhanced_num_of_topics = 128  # int(np.ceil(datasets_helper.get_num_of_topics()*4)) #-datasets_helper.get_num_of_topics()/2))
+    enhanced_num_of_topics = 64  # int(np.ceil(datasets_helper.get_num_of_topics()*4)) #-datasets_helper.get_num_of_topics()/2))
     model.add(Bidirectional(LSTM(enhanced_num_of_topics, return_sequences=True), input_shape=(1, num_of_words)))
-    model.add(Bidirectional(LSTM(enhanced_num_of_topics, return_sequences=True)))
+    model.add(keras.layers.GaussianNoise(0.3))
+    #model.add(Bidirectional(LSTM(enhanced_num_of_topics, return_sequences=True)))
     model.add(Bidirectional(LSTM(enhanced_num_of_topics)))
+    #model.add(keras.layers.LayerNormalization())
     model.add(Dense(datasets_helper.get_num_of_topics(), activation='softmax'))
 
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
     return model
 
-def get_embedding_model(datasets_helper, params=None):
-    embedding_dim = 200
+def get_embedding_glove_model(datasets_helper, params=None,train_embedding=False):
+    embedding_dim = params.get('embedding_dim',50)
+    max_seq_len = params.get('max_seq_len')
     tokenizer = params.get('tokenizer',None)
     model = Sequential()
-    model.add(Embedding(num_of_words, embedding_dim))
+    model.add(Embedding(num_of_words, embedding_dim, input_length=max_seq_len))
+    model.add(keras.layers.GaussianNoise(0.4))
     model.add(LSTM(64, return_sequences=True))
-    model.add(LSTM(128))
+    model.add(Dropout(0.3))
+    model.add(LSTM(64))
+    model.add(keras.layers.LayerNormalization())
     model.add(Dense(datasets_helper.get_num_of_topics(), activation='softmax'))
     model.layers[0].set_weights([get_embedding_matrix(num_of_words, embedding_dim, tokenizer.word_index)])
-    model.layers[0].trainable = False
+    model.layers[0].trainable = train_embedding
 
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
     return model
 
+
+
 def get_embedding_trained_model(datasets_helper, params=None):
+    embedding_dim = params.get('embedding_dim',50)
+    max_seq_len = params.get('max_seq_len')
     model: Sequential = Sequential()
-    model.add(Embedding(num_of_words, 50, input_length=max_seq_len))
-    model.add(LSTM(128))
+    model.add(Embedding(num_of_words, embedding_dim, input_length=max_seq_len))
+    model.add(keras.layers.GaussianNoise(0.3))
+    model.add(LSTM(64, return_sequences=True))
+    model.add(Dropout(0.3))
+    model.add(LSTM(64))
+    model.add(keras.layers.LayerNormalization())
     model.add(Dense(datasets_helper.get_num_of_topics(), activation='softmax'))
 
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
@@ -141,9 +153,9 @@ def get_embedding_trained_model(datasets_helper, params=None):
 def get_text_generator_class(model_type, params=None):
     if model_type in [ModelType.DENSE]:
         return TrainingTextGenerator
-    if model_type in [ModelType.RNN,ModelType.GRU,ModelType.LSTM,ModelType.BIDI_GRU, ModelType.CONV, ModelType.CONV_GRU]:
+    if model_type in [ModelType.RNN,ModelType.GRU,ModelType.LSTM,ModelType.BIDI_GRU,  ModelType.CONV_GRU]:
         return TrainingTextGeneratorRNN
-    if model_type in [ModelType.EMBEDDING_LSTM, ModelType.EMBEDDING_TRAINED_LSTM]:
+    if model_type in [ModelType.CONV, ModelType.EMBEDDING_GLOVE_LSTM, ModelType.EMBEDDING_GLOVE_TRAINED_LSTM, ModelType.EMBEDDING_TRAINED_LSTM]:
         return TrainingTextGeneratorRNNEmbedding
     return TrainingTextGenerator
 
@@ -162,13 +174,15 @@ def get_model_from_type(model_type, datasets_helper, params=None):
         return get_GRU_model(datasets_helper,params)
     elif model_type == ModelType.RNN:
         return get_RNN_model(datasets_helper,params)
-    elif model_type == ModelType.EMBEDDING_LSTM:
-        return get_embedding_trained_model(datasets_helper,params)
     elif model_type == ModelType.EMBEDDING_TRAINED_LSTM:
-        return get_embedding_model(datasets_helper,params)
+        return get_embedding_trained_model(datasets_helper,params)
+    elif model_type == ModelType.EMBEDDING_GLOVE_LSTM:
+        return get_embedding_glove_model(datasets_helper, params)
+    elif model_type == ModelType.EMBEDDING_GLOVE_TRAINED_LSTM:
+        return get_embedding_glove_model(datasets_helper, params,True)
     return Sequential()
 
-tested_model = ModelType.DENSE
+tested_model = ModelType.EMBEDDING_GLOVE_TRAINED_LSTM
 file_dir = os.path.dirname(__file__)
 sys.path.append(file_dir)
 root = tk.Tk()
@@ -181,10 +195,11 @@ log_writer = LogWriter(log_file_desc=simpledialog.askstring(title="Test Name",
 results = []
 num_of_words = 15000
 batch_size = 256
-epochs = 30
+epochs = 50
 val_split = 0.2
-max_seq_len = 300
-tokenizer_mode = 'binary'
+max_seq_len = 400
+embedding_dim = 200
+tokenizer_mode = 'tfidf'
 
 
 datasets_helper.set_wanted_datasets([3])#[0,1,2,3,6]
@@ -196,13 +211,16 @@ while datasets_helper.next_dataset():
     log_writer.add_log("Starting preprocessing and tokenization.")
     tokenizer.fit_on_texts(generator)
     log_writer.add_log("Done. Building model now.")
-
-    model = get_model_from_type(tested_model,datasets_helper)
+    params = {}
+    params['tokenizer'] = tokenizer
+    params['max_seq_len'] = max_seq_len
+    params['embedding_dim'] = embedding_dim
+    model = get_model_from_type(tested_model,datasets_helper,params)
     model.summary()
 
     log_writer.add_log("Done. Preparing generators.")
     log_writer.add_log(
-        'Arguments used were: batch_size={}\nNum_of_epochs={}\ntokenizer_mode={}'.format(batch_size, epochs,tokenizer_mode))
+        'Arguments used were: batch_size={}\nNum_of_epochs={}\nembedding_dim={}\ntokenizer_mode={}'.format(batch_size, epochs,max_seq_len,tokenizer_mode))
     early_stop = EarlyStopping(monitor='val_accuracy', patience=5, restore_best_weights=True)
     text_generator = get_text_generator_class(tested_model)
     train = text_generator(

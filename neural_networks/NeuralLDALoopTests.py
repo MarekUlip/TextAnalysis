e31f74d1723afa663a4736bd499fc0a9265c2f56
MarekUlip <marek.ulip@gmail.com>
2020-05-07 03:57:19 +0200

    Changes considering various tests.

--- neural_networks/NeuralLDALoopTests.py
+++ neural_networks/NeuralLDALoopTests.py
@@ -50,27 +50,104 @@ params_regularize = [  ['L12In_0.001',keras.regularizers.l1_l2(0.001),None],
 params_text_form = [['sigmoidCrossBinary','sigmoid','categorical_crossentropy','binary'],['softmaxBinaryBinary','softmax','binary_crossentropy','binary'],['sigmoidBinaryBinary','sigmoid','binary_crossentropy','binary'],
           ['sigmoidCrossTFIDF','sigmoid','categorical_crossentropy','tfidf'],['softmaxBinaryTFIDF','softmax','binary_crossentropy','tfidf'],['sigmoidBinaryTFIDF','sigmoid','binary_crossentropy','tfidf']]
 
-params = [['sigmoidCrossTFIDF_L12In_0.001','sigmoid','categorical_crossentropy','tfidf',keras.regularizers.l1_l2(0.001),None],
+params_text_form_reg = [['sigmoidCrossTFIDF_L12In_0.001','sigmoid','categorical_crossentropy','tfidf',keras.regularizers.l1_l2(0.001),None],
           ['sigmoidCrossBinary_L12In_0.001','sigmoid','categorical_crossentropy','binary',keras.regularizers.l1_l2(0.001),None],
           ['sigmoidBinaryTFIDF_L12In_0.001','sigmoid','binary_crossentropy','tfidf',keras.regularizers.l1_l2(0.001),None],
           ['sigmoidCrossTFIDF_L12Both_0.001', 'sigmoid', 'categorical_crossentropy', 'tfidf',keras.regularizers.l1_l2(0.001),keras.regularizers.l1_l2(0.001)],
           ['sigmoidCrossBinary_L12Both_0.001', 'sigmoid', 'categorical_crossentropy', 'binary',keras.regularizers.l1_l2(0.001),keras.regularizers.l1_l2(0.001)],
           ['sigmoidBinaryTFIDF_L12Both_0.001', 'sigmoid', 'binary_crossentropy', 'tfidf', keras.regularizers.l1_l2(0.001),keras.regularizers.l1_l2(0.001)]
           ]
-
-params_kernel_regularize = [['Kernel_L12In_0.001',keras.regularizers.l1_l2(0.001),None],
-['Kernel_L1In_0.001',keras.regularizers.l1(0.001),None],
-['Kernel_L12Out_0.001',None,keras.regularizers.l1_l2(0.001)],
-['Kernel_L12Out_0.0001',None,keras.regularizers.l1_l2(0.0001)],
-['Kernel_L12Both_0.001',keras.regularizers.l1_l2(0.001),keras.regularizers.l1_l2(0.001)],
-['Kernel_L12Both_0.0001',keras.regularizers.l1_l2(0.001),keras.regularizers.l1_l2(0.0001)],
-['Kernel_L12Both_0.01',keras.regularizers.l1_l2(0.001),keras.regularizers.l1_l2(0.01)]
+"""['KernelL12In_0.001',keras.regularizers.l1_l2(0.001),None],
+['KernelL12In_0.0001',keras.regularizers.l1_l2(0.0001),None],"""
+params_kernel = [
+['KernelL2In_0.001',keras.regularizers.l2(0.001),None],
+['KernelL2Out_0.001',None,keras.regularizers.l2(0.001)],
+['KernelL12Out_0.001',None,keras.regularizers.l1_l2(0.001)],
+['KernelL12Out_0.0001',None,keras.regularizers.l1_l2(0.0001)],
+['KernelL12Both_0.001',keras.regularizers.l1_l2(0.001),keras.regularizers.l1_l2(0.001)],
+['KernelL12Both_0.0001',keras.regularizers.l1_l2(0.001),keras.regularizers.l1_l2(0.0001)],
+['KernelL12Both_0.01',keras.regularizers.l1_l2(0.001),keras.regularizers.l1_l2(0.01)],
+['KernelL2Both_0.001',keras.regularizers.l2(0.001),keras.regularizers.l2(0.001)]
         ]
+params_ker_ac_combined = [
+['KerAcL2Out_0.001_L12In_0.001',None,keras.regularizers.l2(0.001),keras.regularizers.l1_l2(0.001),None],
+['KerAcL12Out_0.0001_L12In_0.001',None,keras.regularizers.l1_l2(0.0001),keras.regularizers.l1_l2(0.001),None],
+['KerAcL2Out_0.001_L12Both_0.0001',None,keras.regularizers.l2(0.001),keras.regularizers.l1_l2(0.001),keras.regularizers.l1_l2(0.0001)],
+['KerAcL12Out_0.0001_L12Both_0.0001',None,keras.regularizers.l1_l2(0.0001),keras.regularizers.l1_l2(0.001),keras.regularizers.l1_l2(0.0001)],
+        ]
+
+
+#From other PC *****************************************
+
+params_text_form = [['sigmoidCrossBinary', 'sigmoid', 'categorical_crossentropy', 'binary'],
+                    ['softmaxBinaryBinary', 'softmax', 'binary_crossentropy', 'binary'],
+                    ['sigmoidBinaryBinary', 'sigmoid', 'binary_crossentropy', 'binary'],
+                    ['sigmoidCrossTFIDF', 'sigmoid', 'categorical_crossentropy', 'tfidf'],
+                    ['softmaxBinaryTFIDF', 'softmax', 'binary_crossentropy', 'tfidf'],
+                    ['sigmoidBinaryTFIDF', 'sigmoid', 'binary_crossentropy', 'tfidf']]
+
+params_text_form_detailed = [
+    ['sigmoidCrossTFIDF_L12In_0.001', 'sigmoid', 'categorical_crossentropy', 'tfidf', keras.regularizers.l1_l2(0.001),
+     None],
+    ['sigmoidCrossBinary_L12In_0.001', 'sigmoid', 'categorical_crossentropy', 'binary', keras.regularizers.l1_l2(0.001),
+     None],
+    ['sigmoidBinaryTFIDF_L12In_0.001', 'sigmoid', 'binary_crossentropy', 'tfidf', keras.regularizers.l1_l2(0.001),
+     None],
+    ['sigmoidCrossTFIDF_L12Both_0.001', 'sigmoid', 'categorical_crossentropy', 'tfidf', keras.regularizers.l1_l2(0.001),
+     keras.regularizers.l1_l2(0.001)],
+    ['sigmoidCrossBinary_L12Both_0.001', 'sigmoid', 'categorical_crossentropy', 'binary',
+     keras.regularizers.l1_l2(0.001), keras.regularizers.l1_l2(0.001)],
+    ['sigmoidBinaryTFIDF_L12Both_0.001', 'sigmoid', 'binary_crossentropy', 'tfidf', keras.regularizers.l1_l2(0.001),
+     keras.regularizers.l1_l2(0.001)]
+    ]
+
+params_kernel_regularize = [['Kernel_L12In_0.001', keras.regularizers.l1_l2(0.001), None],
+                            ['Kernel_L1In_0.001', keras.regularizers.l1(0.001), None],
+                            ['Kernel_L12Out_0.001', None, keras.regularizers.l1_l2(0.001)],
+                            ['Kernel_L12Out_0.0001', None, keras.regularizers.l1_l2(0.0001)],
+                            ['Kernel_L12Both_0.001', keras.regularizers.l1_l2(0.001), keras.regularizers.l1_l2(0.001)],
+                            ['Kernel_L12Both_0.0001', keras.regularizers.l1_l2(0.001),
+                             keras.regularizers.l1_l2(0.0001)],
+                            ['Kernel_L12Both_0.01', keras.regularizers.l1_l2(0.001), keras.regularizers.l1_l2(0.01)]
+                            ]
+params_bias = [['Bias_L12In_0.001', keras.regularizers.l1_l2(0.001), None],
+               ['Bias_L1In_0.001', keras.regularizers.l1(0.001), None],
+               ['Bias_L12Out_0.001', None, keras.regularizers.l1_l2(0.001)],
+               ['Bias_L12Out_0.0001', None, keras.regularizers.l1_l2(0.0001)],
+               ['Bias_L12Both_0.001', keras.regularizers.l1_l2(0.001), keras.regularizers.l1_l2(0.001)],
+               ['Bias_L12Both_0.0001', keras.regularizers.l1_l2(0.001), keras.regularizers.l1_l2(0.0001)],
+               ['BiasL2Both_0.001', keras.regularizers.l2(0.001), keras.regularizers.l2(0.001)],
+               ['Bias_L12Both_0.01', keras.regularizers.l1_l2(0.001), keras.regularizers.l1_l2(0.01)]
+               ]
+
+params_tfidf = [['TFIDF_L12In_0.001', keras.regularizers.l1_l2(0.001), None],
+                ['TFIDF_L1In_0.001', keras.regularizers.l1(0.001), None],
+                ['TFIDF_L12Out_0.001', None, keras.regularizers.l1_l2(0.001)],
+                ['TFIDF_L12Out_0.0001', None, keras.regularizers.l1_l2(0.0001)],
+                ['TFIDF_L12Both_0.001', keras.regularizers.l1_l2(0.001), keras.regularizers.l1_l2(0.001)],
+                ['TFIDF_L12Both_0.0001', keras.regularizers.l1_l2(0.001), keras.regularizers.l1_l2(0.0001)],
+                ['TFIDFL2Both_0.001', keras.regularizers.l2(0.001), keras.regularizers.l2(0.001)],
+                ['TFIDF_L12Both_0.01', keras.regularizers.l1_l2(0.001), keras.regularizers.l1_l2(0.01)]
+                ]
+params_bin_bin_cross = [['BinBinaryCross_L12In_0.001', keras.regularizers.l1_l2(0.001), None],
+          ['BinBinaryCross_L1In_0.001', keras.regularizers.l1(0.001), None],
+          ['BinBinaryCross_L12Out_0.001', None, keras.regularizers.l1_l2(0.001)],
+          ['BinBinaryCross_L12Out_0.0001', None, keras.regularizers.l1_l2(0.0001)],
+          ['BinBinaryCross_L12Both_0.001', keras.regularizers.l1_l2(0.001), keras.regularizers.l1_l2(0.001)],
+          ['BinBinaryCross_L12Both_0.0001', keras.regularizers.l1_l2(0.001), keras.regularizers.l1_l2(0.0001)],
+          ['BinBinaryCrossL2Both_0.001', keras.regularizers.l2(0.001), keras.regularizers.l2(0.001)],
+          ['BinBinaryCross_L12Both_0.01', keras.regularizers.l1_l2(0.001), keras.regularizers.l1_l2(0.01)]
+          ]
+
+# End *******
+params = [['LDA5',None,keras.regularizers.l2(0.001),keras.regularizers.l1_l2(0.001),None]]
+params = [
+          ['Yelp',6,10]]
 i = 0
 for param in params:
     seed(42)
     tf.random.set_seed(42)
-    test_name = params[0]
+    test_name = param[0]
     i+=1
     #config = tf.compat.v1.ConfigProto( device_count = {'GPU': 1 , 'CPU': 4} )
     #sess = tf.compat.v1.Session(config=config)
@@ -83,7 +160,7 @@ for param in params:
     num_of_words = 10000
 
     dataset_helper = Dataset_Helper(True)
-    dataset_helper.set_wanted_datasets([2])
+    dataset_helper.set_wanted_datasets([param[1]])
     dataset_helper.next_dataset()
     num_of_topics = dataset_helper.get_num_of_topics()
     documents = dataset_helper.get_texts_as_list()
@@ -92,7 +169,7 @@ for param in params:
     tokenizer.fit_on_texts(documents)
     #items= tokenizer.word_index
     reverse_word_map = dict(map(reversed, tokenizer.word_index.items()))
-    matrix = tokenizer.texts_to_matrix(documents, mode=param[3])
+    matrix = tokenizer.texts_to_matrix(documents, mode='binary')
     print(getsizeof(documents))
     print(getsizeof(tokenizer))
     print(getsizeof(matrix))
@@ -108,17 +185,17 @@ for param in params:
     #regularization = 0.001
     input_row = Input(shape=(num_of_words,))
     #encoder = Dense(int(num_of_words/2), activation='relu')(input_row)
-    encoder= Dense(num_of_topics, activation='relu',activity_regularizer=param[4])(input_row)#
+    encoder= Dense(num_of_topics, activation='relu',activity_regularizer=keras.regularizers.l1_l2(0.0001))(input_row)#
     #encoder= keras.layers.LayerNormalization()(encoder)
     #decoder = Dense(int(num_of_words/2), activation='relu')(encoder)
-    output_row = Dense(num_of_words,activation=param[1],activity_regularizer=param[5])(encoder)
+    output_row = Dense(num_of_words,activation='sigmoid',activity_regularizer=keras.regularizers.l1_l2(0.0001))(encoder)
 
     autoencoder = Model(input_row,output_row)
     autoencoder.summary()
     #autoencoder.compile(optimizer='adadelta', loss='mse', metrics=['accuracy'])
-    autoencoder.compile(optimizer='adam', loss=param[2],metrics=['accuracy'])#optimizer='adadelta', loss='mse', metrics=['accuracy'])
+    autoencoder.compile(optimizer='adam', loss='binary_crossentropy',metrics=['accuracy'])#optimizer='adadelta', loss='mse', metrics=['accuracy'])
     #autoencoder.compile(optimizer='adadelta', loss='categorical_crossentropy', metrics=['accuracy'])
-    history = autoencoder.fit(matrix,matrix,batch_size=32,epochs=1000,validation_data=(matrix,matrix), verbose=2, callbacks=[EarlyStopping(monitor='val_loss', min_delta=0, patience=100, verbose=0, mode='auto', baseline=None,
+    history = autoencoder.fit(matrix,matrix,batch_size=32,epochs=15,validation_data=(matrix,matrix), verbose=2, callbacks=[EarlyStopping(monitor='val_loss', min_delta=0, patience=param[2], verbose=0, mode='auto', baseline=None,
                       restore_best_weights=False)])
     weight_in = autoencoder.get_weights()[0]
     weight_out = autoencoder.get_weights()[2]
@@ -164,8 +241,8 @@ for param in params:
     # TODO maybe test work with dict self.dictionary.filter_extremes(no_below=2, no_above=0.5, keep_n=100000)
     doc_term_matrix = [dictionary.doc2bow(doc) for doc in texts]"""
     model = Lda(num_of_topics,num_of_important_words,
-                passes=25,
-                iterations=25)
+                passes=5,
+                iterations=5)
 
     """gensim.models.LdaModel(
     doc_term_matrix,
@@ -182,7 +259,7 @@ for param in params:
         print(topic_words_lda)
         log_writer.write_2D_list('topic_words_lda', topic_words_lda, 'w+')
         test_model(documents, labels, model, log_writer, 'standard_lda')
-        plot_clustering_chart(model,True,documents,log_writer,'lda',dataset_helper.get_dataset_name(),dataset_helper.get_num_of_topics())
+        #plot_clustering_chart(model,True,documents,log_writer,'lda',dataset_helper.get_dataset_name(),dataset_helper.get_num_of_topics())
         measureCoherence(topic_words_lda, log_writer, model.dictionary, documents, 'lda', dataset_helper.get_dataset_name())
     else:
         model.dictionary = corpora.Dictionary([text.split() for text in documents])
